c6e259e2189fea2645e8db140a1698436f2f705f
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-07 10:22:39 -0500

    added comments

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // the constructor always runs first
   constructor() {
     super();
 
@@ -12,6 +13,7 @@ class App extends Component {
     };
   }
 
+  // Brings in the API. mounts the initial state of the render. this runs 3rd.
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
@@ -26,6 +28,7 @@ class App extends Component {
     );
   }
   
+  // render is the template for the HTML..this runs 2nd. after the state changes, this runs again so the page is re-rendered with the new state.
   render() {
   return (
     <div className="App">
