69703151444cfd3db2deb95f344789aa632bca18
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-06 23:06:04 -0500

    added object to state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Charles'
+      name: {firstName: 'Charles', lastName: 'Willcockson'},
+      company: 'Codeup'
     }
   }
   
@@ -17,9 +18,9 @@ class App extends Component {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi {this.state.name}</p>
+        <p>Hi, I am {this.state.name.firstName} {this.state.name.lastName}. I work at {this.state.company}</p>
         <button onClick={() => {
-          this.setState({name: 'Justin'});
+          this.setState({name: {firstName: 'Justin', lastName: 'Wilcox'}});
         }}>Change name</button>
       </header>
     </div>
