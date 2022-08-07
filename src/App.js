2decc55813238c0f0abf076f087ec45f94e14bb1
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-07 12:47:55 -0500

    made component work with export

--- src/App.js
+++ src/App.js
@@ -13,20 +13,15 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
-    console.log('constructor');
   }
 
   // Brings in the API. mounts the initial state of the render. this runs 3rd.
   componentDidMount() {
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(
         () => {
           return { monsters: users };
-        },
-        () => {
-          console.log(this.state);
         }
       )
     );
@@ -41,8 +36,6 @@ class App extends Component {
   
   // render is the template for the HTML..this runs 2nd. after the state changes, this runs again so the page is re-rendered with the new state.
   render() {
-    console.log('render');
-
     const {monsters, searchField } = this.state;
     const { onSearchChange } = this;
     const filteredMonsters = monsters.filter((monster) => {
@@ -61,7 +54,7 @@ class App extends Component {
           return <h1 key={monster.id}>{monster.name}</h1>
         })
       } */}
-      <CardList />
+      <CardList monsters={filteredMonsters}/>
     </div>
   );
   }
