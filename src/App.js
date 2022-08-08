8f5f54a38f1606558017e37ee31d38c6d330360a
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-08 11:39:22 -0500

    added useEffect hooks into functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react';
+import { Component, useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import logo from './logo.svg';
@@ -8,21 +8,26 @@ import './App.css';
 const App = () => {
   const [searchField, setSearchField] = useState('');  //we get back [value, setValue]
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  
 
-  fetch('https://jsonplaceholder.typicode.com/users')
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => setMonsters(users)
-    );
+      .then((users) => setMonsters(users));
+    }, []);
+
+    useEffect(() => {
+      const newFilteredMonsters = monsters.filter((monster) => {
+        return monster.name.toLocaleLowerCase().includes(searchField);
+      });
+      setFilteredMonsters(newFilteredMonsters);
+    }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
       const searchFieldString = event.target.value.toLocaleLowerCase();
       setSearchField(searchFieldString);
     }
-  
-
-  const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
 
   return(
     <div className="App">
